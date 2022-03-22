78432180c38b1596129833a12753879b62993653
AdershBabu <65764036+AdershBabu@users.noreply.github.com>
2022-03-23 00:48:26 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,17 @@
+function fun_1(){
+    var content=document.getElementById("comment").value;
+    console.log(content);
+    window.alert("Thank you for your feedback");
+   
+}
+function alert_1(){
+    
+    document.getElementById("img1").src='C:/Users/User/Desktop/my website/download.png';
+     
+
+}
+function alert_2(){
+   
+    document.getElementById("img1").src='C:/Users/User/Desktop/my website/photo-1532007271951-c487760934ae.png';
+    
+}
